10ef22621aac16f9e90df27942f3d564160fb005
ben.Hung1 <ga2006206521@gmail.com>
2022-08-20 01:46:45 +0800

    feat/record - 100%

--- js/record.js
+++ js/record.js
@@ -40,82 +40,164 @@ $(document).ready(function () {
   vm = new Vue(app);
 });
 
-// 從這開始
+// 從這開始 從這開始 從這開始 從這開始 從這開始
+
+let userId;
+
+function getUserId () {
+  let userNumber = document.getElementById("userNumberInput").value;
+  if (!userNumber) {
+    alert('請不要空白')
+    return false;
+  } else {
+    fetch('https://event.setn.com/api/2022moonTest/signin', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: `${userNumber}`,
+      }),
+    })
+    .then((data) => {
+      return data.json()
+    }).then((finalData)=> {
+      localStorage.setItem('userId', finalData.id)
+      alert('登入成功')
+      popupClosed();
+      document.getElementById('userLoginNumber').style.display = 'none';
+      document.getElementById('userLoginEventNumber').style.display = 'block'
+      document.getElementById('userLoginPoint').style.display = 'block'
+      document.getElementById('userLoginEventNumberValue').innerText = finalData.id
+      document.getElementById('userLoginPointValue').innerText = finalData.point
+      getUserPointResult()
+    }).catch((error) => {
+      console.log(error);
+      alert("請輸入正確的活動編號");
+      return false;
+    })  
+  }
+}
+
+
+
+// 會員編號彈窗
+
+function memberNumber () {
+  document.getElementById('iphonepage').style.display = 'block'
+  document.getElementById('popup').style.display = 'none'
+}
+
+// 注意事項彈窗
+
+function popupOpen() {
+  document.getElementById('fade').style.display = 'block';
+  document.getElementsByClassName('popup')[0].style.display = 'block'
+  document.getElementById('noticepage').style.display = 'block'
+}
+
+// 關閉彈窗
+
+function popupClosed() {
+  document.getElementById('popup').style.display = 'none'
+  document.getElementsByClassName('popup')[0].style.display = 'none'
+  document.getElementById('noticepage').style.display = 'none'
+  document.getElementById('fade').style.display = 'none';
+  document.getElementById('iphonepage').style.display = 'none'
+}
+
+// 登入活動編號 彈窗
+
+function getUserLogin () {
+  document.getElementById('fade').style.display = 'block';
+  document.getElementById('popup').style.display = 'block'
+}
 
 function getUserPointResult() {
-  document.getElementById("Abox").style.display = "block";
-  document.getElementById("Bbox").style.display = "none";
 
-  let pointResult = document.getElementById("pointResult");
+  userId = localStorage.getItem('userId')
 
-  fetch("https://event.setn.com/api/2022moonTest/point/histories")
-    .then((data) => {
-      return data.json();
-    })
-    .then((finalData) => {
-      console.log(finalData);
-      localStorage.setItem("pointResults", JSON.stringify(finalData));
-    })
-    .catch((error) => {
-      console.log(error);
+  if(userId !== '') {
+    document.getElementById("Abox").style.display = "block";
+    document.getElementById("Bbox").style.display = "none";
+  
+    let pointResult = document.getElementById("pointResult");
+  
+    fetch("https://event.setn.com/api/2022moonTest/point/histories")
+      .then((data) => {
+        return data.json();
+      })
+      .then((finalData) => {
+        console.log(finalData);
+        localStorage.setItem("pointResults", JSON.stringify(finalData));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  
+    userFinalPointResults = JSON.parse(localStorage.getItem("pointResults"));
+  
+    let finalUserPoints = "";
+    userFinalPointResults.histories.map((val) => {
+      finalUserPoints += `
+          <li class="pointsbox">
+          <div class="box-left">
+            <span id="pointDate">${val.date.substring(5, 10)}</span>
+            <p id="desc">${val.description}</p>
+          </div>
+          <div class="box-right">
+            <p id="point"><strong></strong>${val.point}點</p>
+          </div>
+        </li>
+          `;
     });
-
-  userFinalPointResults = JSON.parse(localStorage.getItem("pointResults"));
-
-  let finalUserPoints = "";
-  userFinalPointResults.histories.map((val) => {
-    finalUserPoints += `
-        <li class="pointsbox">
-        <div class="box-left">
-          <span id="pointDate">${val.date.substring(5, 10)}</span>
-          <p id="desc">${val.description}</p>
-        </div>
-        <div class="box-right">
-          <p id="point"><strong></strong>${val.point}點</p>
-        </div>
-      </li>
-        `;
-  });
-
-  pointResult.innerHTML = finalUserPoints;
+    pointResult.innerHTML = finalUserPoints;
+  }
 }
 
 function getUserTurnTableResult() {
-  document.getElementById("Abox").style.display = "none";
-  document.getElementById("Bbox").style.display = "block";
-  fetch("https://event.setn.com/api/2022moonTest/spinToWin/histories", {
-    method: "POST",
-    mode: "cors",
-  })
-    .then((data) => {
-      return data.json();
+
+  userId = localStorage.getItem('userId')
+
+
+  if(userId !== '') {
+    document.getElementById("Abox").style.display = "none";
+    document.getElementById("Bbox").style.display = "block";
+    fetch("https://event.setn.com/api/2022moonTest/spinToWin/histories", {
+      method: "POST",
+      mode: "cors",
     })
-    .then((finalData) => {
-      console.log(finalData);
-      localStorage.setItem("turnTableResults", JSON.stringify(finalData));
-    })
-    .catch((error) => {
-      console.log(error);
+      .then((data) => {
+        return data.json();
+      })
+      .then((finalData) => {
+        console.log(finalData);
+        localStorage.setItem("turnTableResults", JSON.stringify(finalData));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  
+    userFinalTurnTableResults = JSON.parse(
+      localStorage.getItem("turnTableResults")
+    );
+  
+    let finalUserTurnTable = "";
+    userFinalTurnTableResults.histories.map((val) => {
+      finalUserTurnTable += `
+      <li class="pointsbox">
+      <div class="box-left">
+        <span>${val.date.substring(5, 10)}</span>
+        <b class="join-item">參加轉盤兌換</b>
+        <b>${val.description}</b>
+      </div>
+      <div class="box-right">
+        <p><strong>${val.point}</strong>點</p>
+      </div>
+    </li>
+  `;
     });
-
-  userFinalTurnTableResults = JSON.parse(
-    localStorage.getItem("turnTableResults")
-  );
-
-  let finalUserTurnTable = "";
-  userFinalTurnTableResults.histories.map((val) => {
-    finalUserTurnTable += `
-    <li class="pointsbox">
-    <div class="box-left">
-      <span>${val.date.substring(5, 10)}</span>
-      <b class="join-item">參加轉盤兌換</b>
-      <b>${val.description}</b>
-    </div>
-    <div class="box-right">
-      <p><strong>${val.point}</strong>點</p>
-    </div>
-  </li>
-`;
-  });
-  turnTableResult.innerHTML = finalUserTurnTable;
+    turnTableResult.innerHTML = finalUserTurnTable;
+  }
 }
