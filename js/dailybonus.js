2791cdd26e32d07969be9a193c1945ce5289d8ef
ben.Hung1 <ga2006206521@gmail.com>
2022-08-21 23:32:55 +0800

    feat/dailySignIn
    從答案api開始做

--- js/dailybonus.js
+++ js/dailybonus.js
@@ -73,6 +73,14 @@ $(document).ready(function() {
 
 let userId;
 
+let siginFinished ;
+
+let quizId;
+
+
+
+
+
 function getUserId () {
   let userNumber = document.getElementById("userNumberInput").value;
   if (!userNumber) {
@@ -92,6 +100,7 @@ function getUserId () {
     .then((data) => {
       return data.json()
     }).then((finalData)=> {
+        console.log(finalData);
       localStorage.setItem('userId', finalData.id)
       alert('登入成功')
       popupClosed();
@@ -100,13 +109,84 @@ function getUserId () {
       document.getElementById('userLoginPoint').style.display = 'block'
       document.getElementById('userLoginEventNumberValue').innerText = finalData.id
       document.getElementById('userLoginPointValue').innerText = finalData.point
-    }).catch((error) => {
+      dailyBonusGetStared();
+      for(let i = 0; i < finalData.signin.histories.length; i++ ) {
+        // console.log(finalData.signin.histories[i]);
+        // console.log(document.getElementsByClassName('normal-box')[i]); 
+        if(finalData.signin.histories[i].signed) {
+            document.getElementsByClassName('normal-box')[i].classList.add('finished')
+            siginFinished = document.getElementsByClassName('finished')[i]
+            // console.log(siginFinished.parentNode.nextElementSibling);
+        }
+        document.getElementById('signtoday-btn').addEventListener('click', function() {
+            document.getElementById('signtoday-btn').style.display = 'none';
+            document.getElementById('signtomorrow-btn').style.display = 'block';
+            siginFinished.parentNode.nextElementSibling.classList.add('finished');
+            if(siginFinished.parentNode.nextElementSibling.firstElementChild.innerText == 'DAY 3' || siginFinished.parentNode.nextElementSibling.firstElementChild.innerText == 'DAY 6' || siginFinished.parentNode.nextElementSibling.firstElementChild.innerText == 'DAY 9' || siginFinished.parentNode.nextElementSibling.firstElementChild.innerText == 'DAY 12' || siginFinished.parentNode.nextElementSibling.firstElementChild.innerText == 'DAY 15' || siginFinished.parentNode.nextElementSibling.firstElementChild.innerText == 'DAY 18' || siginFinished.parentNode.nextElementSibling.firstElementChild.innerText == 'DAY 21') {
+                document.getElementById('eggpopup').style.display = 'block';
+                document.getElementById('fade').style.display = 'block';
+                document.getElementById('surprisePoints').innerText = 'finalData.bouns.point點'
+            }
+        })
+    }    
+  }).catch((error) => {
       console.log(error);
       alert("請輸入正確的活動編號");
       return false;
     })  
   }
 }
+
+function getNewsPopup () {
+    document.getElementById('newsPopup').style.display = 'block'
+    document.getElementById('fade').style.display = 'block';
+    fetch('https://event.setn.com/api/2022moonTest/quiz')
+    .then((data) => {
+        return data.json();
+    }).then((finalData) => {
+        console.log(finalData);
+        document.getElementById('question').innerText = `Q. ${finalData[0].text}`;
+        document.getElementById('answerA').innerText = `A. ${finalData[0].options[0]}`;
+        document.getElementById('answerB').innerText = `B. ${finalData[0].options[1]}`;
+        document.getElementById('openBook').addEventListener('click', function() {
+            window.open(finalData[0].referrence)
+        })
+        localStorage.setItem('quizId', finalData[0].id)
+        // localStorage.setItem('quizAnsA', )
+    }).catch((error) => {
+        console.log(error);
+    })
+}
+
+function getNewsAnswerA () {
+
+    quizId = localStorage.getItem('quizId');
+
+    fetch('https://event.setn.com/api/2022moonTest/quiz', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: `${quizId}`,
+          }),
+    })
+}
+
+
+
+function dailyBonusGetStared () {
+    userId = localStorage.getItem('userId');
+    // if(userId !== '' ) {
+        document.getElementById('loginbtn-start').style.display = 'none';
+        document.getElementById('signtoday-btn').style.display = 'block';
+
+        document.getElementById('newsbtn-exam grey').style.display = 'none';
+        document.getElementById('newsbtn-exam').style.display ='block';
+
+    // }
+  }
 
 
 
@@ -133,6 +213,8 @@ function popupClosed() {
   document.getElementById('noticepage').style.display = 'none'
   document.getElementById('fade').style.display = 'none';
   document.getElementById('iphonepage').style.display = 'none'
+  document.getElementById('eggpopup').style.display ='none';
+  document.getElementById('newsPopup').style.display = 'none';
 }
 
 // 登入活動編號 彈窗
--- js/turntable.js
+++ js/turntable.js
@@ -146,145 +146,19 @@ function operation(ran) {
   }
 }
 
-// var app = {
-//   el: "#app",
-//   data() {
-//     return {
-//       name: "2022classTest",
-//       apiBaseUrl: "https://event.setn.com/ci",
-//       domain: "acts.setn.com",
-//       user: {
-//         id: "",
-//         name: "",
-//         memberId: "",
-//         inputMemberId: "",
-//         contact: "",
-//         point: 0,
-//         reward: "",
-//         draw: false,
-//       },
-//       tableStep: 0,
-//       turnOn: false,
-//       drawChance: 0,
-//       baseDeduct: 10,
-//       /* NOTICE | LOGIN | MYID | QUIZ | SURPRISE */
-//       dialog: "",
-//       /* HOME */
-//       section: "",
-//       reloginBtn: false,
-//     };
-//   },
-//   computed: {},
-//   mounted() {},
-//   methods: {},
-// };
+// 注意事項彈窗
 
-// $(document).ready(function () {
-//   // if (document.location.protocol == "http:") {
-//   //     window.location.replace(window.location.href.replace("http:", "https:"));
-//   // }
 
-//   vm = new Vue(app);
-// });
+function popupOpen() {
+  document.getElementById('fade').style.display = 'block';
+  document.getElementsByClassName('popup')[0].style.display = 'block'
+  document.getElementById('noticepage').style.display = 'block'
+}
 
-// // 從這開始
+// 關閉彈窗
 
-// // 進來先打第一隻api
+function popupClosed() {
+  document.getElementById('noticepage').style.display = 'none'
+  document.getElementById('fade').style.display = 'none';
+}
 
-// let finalPrize;
-
-// let quota;
-
-// window.onload = function () {
-//   getQuota();
-//   getfinalPrize();
-// };
-
-// async function getQuota() {
-//   await fetch("https://event.setn.com/api/2022moonTest/spinToWin")
-//     .then((data) => {
-//       return data.json();
-//     })
-//     .then((finalData) => {
-//       console.log(finalData);
-//       localStorage.setItem("firstQuota", finalData.spinToWin.quota);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// }
-
-// async function getfinalPrize() {
-//   await fetch("https://event.setn.com/api/2022moonTest/spinToWin", {
-//     method: "POST",
-//   })
-//     .then((data) => {
-//       return data.json();
-//     })
-//     .then((finalData) => {
-//       console.log(finalData);
-
-//       finalPrize = finalData.spinToWin.win.name;
-//       localStorage.setItem("finalQuota", finalData.spinToWin.quota);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// }
-
-// PrizeSon = [
-//   "燒肉眾二代目500元券",
-//   "燒肉哦爺500元券",
-//   "鹿兒島燒肉500元券",
-//   "燒肉眾500元券",
-//   "再接再厲",
-//   "3D優質按摩滾筒",
-//   "7-11壹佰元禮券",
-//   "電動按摩筋膜槍",
-// ];
-// totalNum = 8; // 轉盤 總數
-// turnNum = [1, 2, 3, 4, 5, 6, 7, 8]; //概率 獎品編號
-// isStatr = false; //鎖住 不能重複點擊;
-// lenCloc = 0;
-// turn = 4; //轉盤最低幾圈 你想幾圈
-
-// // 取出剛進頁面的扣打
-
-// let firstQuota = localStorage.getItem("firstQuota");
-// document.getElementById("count").innerText = firstQuota;
-
-// //開始
-// function start(item) {
-//   if (!isStatr && firstQuota > 0) {
-//     isStatr = true;
-//     random = PrizeSon.indexOf(item);
-//     operation(random);
-//   } else {
-//     alert("您剩餘次數不足");
-//     return false;
-//   }
-// }
-
-// function operation(ran) {
-//   let wheel = document.getElementById("wheel");
-//   lenCloc++;
-//   let Prize = turnNum[ran] - 1;
-//   let sun = turn * 360; //最小圈数
-//   if (Prize >= totalNum) {
-//     Prize = 0;
-//   }
-//   if (firstQuota > 0) {
-//     wheel.style.transform = "rotate(" + (lenCloc * sun - Prize * 45) + "deg)";
-//     setTimeout(
-//       function () {
-//         alert(PrizeSon[Prize]); //傳什麼給你什麼
-//         isStatr = false;
-//         firstQuota--;
-//         // 取出轉完的扣打
-//         let finalQuota = localStorage.getItem("finalQuota");
-//         document.getElementById("count").innerText = finalQuota;
-//       }.bind(this),
-//       3000
-//     );
-//   }
-// }
